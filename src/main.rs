3908bfa560d0ddb9e5c29008610065519bbb3722
oshiro <yukidora6464@gmail.com>
2025-04-11 02:35:29 +0900

    LDAのテスト作成

--- src/main.rs
+++ src/main.rs
@@ -46,11 +46,40 @@ pub struct CPU {
                     if self.register_a & 0b1000_0000 != 0 {
                         self.status = self.status | 0b1000_0000;
                     } else {
-                        self.status = self.status & 0b0111_1101;
+                        self.status = self.status & 0b0111_1111;
                     }
+                }
+                
+                // BRK - Loop Break
+                0x00 => {
+                    return;
                 }
                 _ => todo!(),
             }
         }
     } 
+ }
+
+
+ #[cfg(test)]
+ mod test {
+    use super::*;
+
+    #[test]
+    // フラグが立たないLDAテスト
+    fn test_0xa9_lda_immediate_load_data() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        assert_eq!(cpu.register_a, 0x05);
+        assert!(cpu.status & 0b0000_0010 == 0b00);
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    // zeroフラグが立つLADテスト
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
  }
---
+++ target/debug/deps/NES-fe14cb08353bc65a.d
@@ -0,0 +1,5 @@
+C:\Users\ms211111\Documents\NES\target\debug\deps\NES-fe14cb08353bc65a.exe: src\main.rs
+
+C:\Users\ms211111\Documents\NES\target\debug\deps\NES-fe14cb08353bc65a.d: src\main.rs
+
+src\main.rs:
