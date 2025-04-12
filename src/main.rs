0474736b5c2a4da8c4ede287f6a115f9559d8247
oshiro <yukidora6464@gmail.com>
2025-04-13 00:17:46 +0900

    STA実装

--- src/main.rs
+++ src/main.rs
@@ -148,7 +148,7 @@ impl CPU {
             self.program_counter += 1;
 
             match opscode {
-                // LDA - Load Accumulator
+                /* LDA */ 
                 0xA9 => {
                     self.lda(&AddressingMode::Immediate);
                     self.program_counter += 1;
@@ -188,6 +188,37 @@ impl CPU {
                 // TAX - Transfer Accumulator to X
                 0xAA => self.tax(),
                 0xe8 => self.inx(),
+
+                /* STA */ 
+                0x85 => {
+                    self.sta(&AddressingMode::ZeroPage);
+                    self.program_counter += 1;
+                }
+                0x95 => {
+                    self.sta(&AddressingMode::ZeroPage_X);
+                    self.program_counter += 1;
+                }
+                0x8D => {
+                    self.sta(&AddressingMode::Absolute);
+                    self.program_counter += 2;
+                }
+                0x9D => {
+                    self.sta(&AddressingMode::Absolute_X);
+                    self.program_counter += 2;
+                }
+                0x99 => {
+                    self.sta(&AddressingMode::Absolute_Y);
+                    self.program_counter += 2;
+                }
+                0x81 => {
+                    self.sta(&AddressingMode::Indirect_X);
+                    self.program_counter += 1;
+                }
+                0x91 => {
+                    self.sta(&AddressingMode::Indirect_Y);
+                    self.program_counter += 1;
+                }
+                
                 _ => todo!(),
             }
         }
@@ -200,17 +231,20 @@ impl CPU {
         self.register_a = value;
         self.update_zero_and_negative_flags(self.register_a);
     }
-
     // アキュムレータの値をregister_xにコピー
     fn tax(&mut self) {
         self.register_x = self.register_a;
         self.update_zero_and_negative_flags(self.register_x);
     }
-
     // register_xをインクリメント
     fn inx(&mut self) {
         self.register_x = self.register_x.wrapping_add(1);
         self.update_zero_and_negative_flags(self.register_x);
+    }
+    // register_aの値をmemoryに書き込み
+    fn sta(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        self.mem_write(addr, self.register_a);
     }
 
     // ゼロフラグとネガティブフラグ変更
@@ -284,5 +318,14 @@ mod test {
         cpu.load_and_run(vec![0xa9, 0xff, 0xaa, 0xe8, 0x00]); // LDA #$FF, TAX, INX, BRK
         assert_eq!(cpu.register_x, 0x00); // wrap around
         assert!(cpu.status & 0b0000_0010 != 0); // Zero flag should be set
+    }
+
+    #[test]
+    fn test_lda_from_memory() {
+        let mut cpu = CPU::new();
+        cpu.mem_write(0x10, 0x55);
+        cpu.load_and_run(vec![0xa5, 0x10, 0x00]);
+
+        assert_eq!(cpu.register_a, 0x55);
     }
 }
